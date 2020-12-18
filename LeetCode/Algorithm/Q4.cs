20ca58b7b99489621a8195eb226e5403006e37e2
雍杰 邓 <dyj394041771@sina.com>
2020-12-18 20:37:46 +0800

    11

---
+++ LeetCode/Algorithm/Q4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithm
+{
+    public class Q4 : IExecuteTest
+    {
+        public bool Test()
+        {
+            return false;
+        }
+
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int nums1Index = 0;
+            int nums2Index = 0;
+
+            return 0;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -12,7 +12,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Test.Execute("Q738",TestType.Algorithm);
+            Test.Execute("Q389",TestType.Algorithm);
         }
 
         public static class Test
